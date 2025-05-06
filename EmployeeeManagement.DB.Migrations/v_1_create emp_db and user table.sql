d1885c3eda157bc2da90410623acf3c05fa6dc90
durgapenti <durgapenti1234@gmail.com>
2025-05-06 16:22:38 +0530

    emplm-1004-create-db-table-user:DP VP

--- EmployeeeManagement.DB.Migrations/v_1_create emp_db and user table.sql
+++ EmployeeeManagement.DB.Migrations/v_1_create emp_db and user table.sql
@@ -1,10 +1,6 @@
--- Step 1: Create the database if it doesn't exist
+
 CREATE DATABASE IF NOT EXISTS emp_db;
-
--- Step 2: Switch to the database
 USE emp_db;
-
--- Step 3: Create the user table
 CREATE TABLE IF NOT EXISTS emp_db.user (
     userid INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
