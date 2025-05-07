6d3211b701fbafe52c7c70fbce12ad2863bd59f4
durgapenti <durgapenti1234@gmail.com>
2025-05-07 14:34:18 +0530

    emplm-1003 : API :Create API that validaes the username and password :DP VP

---
+++ EmployeeeManagement.API/EmployeeManagementApplication/src/main/java/com/example/emp/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.emp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.emp.model.User;
+import com.example.emp.repository.UserRepository;
+
+@Service
+public class UserService {
+@Autowired
+private UserRepository userRepository;
+
+public User validateUser(String username, String password) {
+User user = userRepository.findByUsername(username);
+if (user != null && user.getPassword().equals(password)) {
+return user;
+}
+return null;
+}
+}
