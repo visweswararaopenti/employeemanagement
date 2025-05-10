5b848b42d8935ceb60e56d2cc384c545532c6c0a
durgapenti <durgapenti1234@gmail.com>
2025-05-10 11:52:52 +0530

    emplm:1025 :Add new user to loginpage :DP VP

--- EmployeeeManagement.API/EmployeeManagementApplication/src/main/java/com/example/emp/controller/LoginController.java
+++ EmployeeeManagement.API/EmployeeManagementApplication/src/main/java/com/example/emp/controller/LoginController.java
@@ -7,21 +7,30 @@ import org.springframework.web.bind.annotation.*;
 import com.example.emp.model.User;
 import com.example.emp.service.UserService;
 
-@CrossOrigin(origins = "*")  
+@CrossOrigin(origins = "*")
 @RestController
 public class LoginController {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @PostMapping("/api/login")
-    public ResponseEntity<String> login(@RequestBody User usermodel) {
-        User user = userService.validateUser(usermodel.getUsername(), usermodel.getPassword());
-        if (user != null) {
-            return ResponseEntity.ok("Login successful for user: " + user.getUsername());
-        } else {
-            return ResponseEntity.status(401).body("Invalid username or password");
-        }
-    }
+	@PostMapping("/api/login")
+	public ResponseEntity<String> login(@RequestBody User usermodel) {
+		User user = userService.validateUser(usermodel.getUsername(), usermodel.getPassword());
+		if (user != null) {
+			return ResponseEntity.ok("Login successful for user: " + user.getUsername());
+		} else {
+			return ResponseEntity.status(401).body("Invalid username or password");
+		}
+	}
 
+	@PostMapping("/api/signup")
+	public ResponseEntity<String> signup(@RequestBody User userModel) {
+		if (userService.existsByUsername(userModel.getUsername())) {
+			return ResponseEntity.status(409).body("Username already exists.");
+		}
+
+		userService.registerUser(userModel);
+		return ResponseEntity.ok("User registered successfully.");
+	}
 }
