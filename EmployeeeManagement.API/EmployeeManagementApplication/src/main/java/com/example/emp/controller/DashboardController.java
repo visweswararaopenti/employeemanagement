b480d6cdb0caadb7623f8551365b5d2904105bfc
durgapenti <durgapenti1234@gmail.com>
2025-05-12 17:41:04 +0530

    emplm:1027 :fix the origin-DP VP

--- EmployeeeManagement.API/EmployeeManagementApplication/src/main/java/com/example/emp/controller/DashboardController.java
+++ EmployeeeManagement.API/EmployeeManagementApplication/src/main/java/com/example/emp/controller/DashboardController.java
@@ -1,5 +1,3 @@
-
-
 package com.example.emp.controller;
 
 import com.example.emp.dto.DashboardSummaryDTO;
@@ -7,8 +5,9 @@ import com.example.emp.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@CrossOrigin(origins = "*") 
 @RequestMapping("/api/dashboard")
+@RestController
 public class DashboardController {
 
     @Autowired
