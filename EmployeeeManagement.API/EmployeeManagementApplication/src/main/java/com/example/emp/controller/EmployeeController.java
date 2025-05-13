ca38a0c879e7cc7121603f56f5393fa252c1ce35
durgapenti <durgapenti1234@gmail.com>
2025-05-13 17:23:58 +0530

    emplm-1008-Add Employee:DP VP

--- EmployeeeManagement.API/EmployeeManagementApplication/src/main/java/com/example/emp/controller/EmployeeController.java
+++ EmployeeeManagement.API/EmployeeManagementApplication/src/main/java/com/example/emp/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,4 +36,11 @@ public class EmployeeController {
 	public ResponseEntity<List<Employee>> getAllEmployees() {
 		return ResponseEntity.ok(employeeService.getAllEmployees());
 	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+	    Employee savedEmployee = employeeService.saveEmployee(employee);
+	    return ResponseEntity.ok(savedEmployee);
+	}
+
 }
